26fe0666239ae6703c2a9200fddfb4e8028df527
oBreez <81740112+oBreez@users.noreply.github.com>
2022-11-19 21:19:01 +0530

    change python version

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "PopCatAPIWrapper"
 copyright = "2022, Infernum1"
 author = "Infernum1"
-release = "0.0.4"
+release = "0.0.5"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = 'furo'
