0845671fbafb9b9a4ef4404e2a18e72e584c3b9a
oBreez <81740112+oBreez@users.noreply.github.com>
2022-11-19 20:12:00 +0530

    add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -6,11 +6,8 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "PopCatAPIWrapper"
 copyright = "2022, Infernum1"
 author = "Infernum1"
-
 release = "0.0.4"
-
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
-
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
 html_theme = 'furo'
+html_favicon = "favicon.ico"
