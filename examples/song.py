6040b06ae3b9d00871348ca6b9eeb5a1eefd9b6f
oBreez <81740112+oBreez@users.noreply.github.com>
2022-11-21 12:58:01 +0530

    fix example

--- examples/song.py
+++ examples/song.py
@@ -7,10 +7,10 @@ client = PopCatAPIWrapper.client.PopCatAPI()
 async def get_song(song_name: str):
     song_name = song_name.replace(" ", "+")
     song = await client.get_song_info(song=song_name)
-    print(song.thumbnail_url)
+    print(song.thumbnail)
     print(song.artist)
     print(song.lyrics)
 
 
 if __name__ == "__main__":
-    asyncio.run(get_song("my little pony"))
+    asyncio.run(get_song("Joker and the queen"))
